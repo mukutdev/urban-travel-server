afdd88254a759de5fe200d20aea7f4be0c4de539
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-08 13:37:01 +0600

    made get all service name api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
@@ -22,9 +22,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
 
     try{
-        await client.connect();
-    console.log('db connection established');
+        
+        const tripCollections = client.db('travel-services').collection('trip-name')
+       
 
+        //get all trips
+
+        app.get('/trips' , async(req , res)=>{
+
+            const query = {}
+            const cursor = tripCollections.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+            console.log(result);
+        })
+
+        // get single trips details
+
+        app.get('/trips/:id' , async(req , res)=>{
+
+            const id = req.params.id
+            const query = {_id : ObjectId(id)}
+            const singleTrip = await tripCollections.findOne(query)
+            res.send(singleTrip)
+            console.log(singleTrip);
+
+
+        })
 
     }
     finally{
