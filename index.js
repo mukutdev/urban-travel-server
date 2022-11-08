f86568087f5cc17990ea524ab8f1df07f998a1de
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-08 12:15:28 +0600

    db connection established

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+require('dotenv').config()
 
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+// mondgoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h5qu391.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+//crud function
+
+async function run(){
+
+    try{
+        await client.connect();
+    console.log('db connection established');
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(err => console.log(err))
 
 
 
