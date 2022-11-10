19af6e78fecd7eb9945dba6822e81042833b3fe6
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-10 11:56:10 +0600

    email base query added

--- index.js
+++ index.js
@@ -1,129 +1,110 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT || 5000
-require('dotenv').config()
-
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 // mondgoDB
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h5qu391.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 //crud function
 
-async function run(){
+async function run() {
+  try {
+    const tripCollections = client
+      .db("travel-services")
+      .collection("trip-name");
+    const reviewCollections = client
+      .db("travel-services")
+      .collection("allReviews");
 
-    try{
+    //get all trips
 
-        const tripCollections = client.db('travel-services').collection('trip-name')
-        const reviewCollections= client.db('travel-services').collection('allReviews')
-       
+    app.get("/trips", async (req, res) => {
+      const query = {};
+      const cursor = tripCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      console.log(result);
+    });
 
-        //get all trips
+    // get 3 trips information
 
-        app.get('/trips' , async(req , res)=>{
+    app.get("/trips/upcoming", async (req, res) => {
+      const query = {};
+      const cursor = tripCollections.find(query);
+      const result = await cursor.limit(3).toArray();
+      res.send(result);
+      console.log(cursor);
+    });
 
-            const query = {}
-            const cursor = tripCollections.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
-            console.log(result);
-        })
+    // get single trips details
 
-        // get 3 trips information
+    app.get("/trips/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singleTrip = await tripCollections.findOne(query);
+      res.send(singleTrip);
+    });
 
-        app.get('/trips/upcoming' , async(req , res)=>{
-            
-        
-            const query = {}
-            const cursor = tripCollections.find(query)
-            const result = await cursor.limit(3).toArray()
-            res.send(result)
-            console.log(cursor);
+    // post reviews
 
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollections.insertOne(review);
+      res.send(result);
+    });
+    //get trip review by specific email
 
-        })
+    app.get("/reviews", async (req, res) => {
+      console.log(req.query);
+      let query = {};
 
-        // get single trips details
+      if(req.query.email){
+          query = {
+              email : req.query.email
+          }
+      }
+      const cursor = reviewCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        app.get('/trips/:id' , async(req , res)=>{
+    //get trip review by specific trip name
 
-            const id = req.params.id
-            const query = {_id : ObjectId(id)}
-            const singleTrip = await tripCollections.findOne(query)
-            res.send(singleTrip)
+    app.get("/reviews", async (req, res) => {
+      console.log(req.query.id);
+      let query = {};
 
-
-
-        })
-
-
-        // post reviews
-        
-        app.post('/reviews' , async(req , res) => {
-
-            const review = req.body
-            const result = await reviewCollections.insertOne(review)
-            res.send(result)
-
-        })
-
-        
-        //get trip review by specific trip name
-
-        app.get('/reviews' , async (req, res) => {
-            console.log(req.query.id);
-            let query = {}
-
-            if(req.query.id){
-                query = {
-                    id : req.query.id
-                }
-            }
-            const cursor = reviewCollections.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
-        })
-
-
-    }
-    finally{
-
-    }
-
+      if (req.query.id) {
+        query = {
+          id: req.query.id,
+        };
+      }
+      const cursor = reviewCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(err => console.log(err))
+run().catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Nice Server is runing on port");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/' , (req , res) => {
-
-    res.send('Nice Server is runing on port')
-})
-
-app.listen(port , ()=>{
-    console.log('server is up and runing' , port);
-})
+app.listen(port, () => {
+  console.log("server is up and runing", port);
+});
